c17e64f0823807c2ce37455494e3d187f9cca523
Grzegorz Kossowski <Grzegorz.Kossowski@action.pl>
2026-02-23 14:40:28 +0100

    Basic landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import { Features } from "@/components/landingpage/features";
+import Footer from "@/components/landingpage/footer";
+import { Hero } from "@/components/landingpage/hero";
+import Pricing from "@/components/landingpage/pricing";
+import Testimonial from "@/components/landingpage/testimonial";
 import Navbar from "@/components/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-     <Navbar/>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <Pricing />
+      <Footer />
+      {/* <FAQ />
+       */}
     </>
   );
 }
---
+++ components/landingpage/hero.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import { ArrowDownRightIcon, ArrowUpRightIcon, CirclePlayIcon } from "lucide-react";
+import Image from "next/image";
+
+export const Hero = () => {
+  return (
+    <div className="min-h-[calc(100vh-4rem)] w-full flex items-center justify-center overflow-hidden border-b border-accent">
+      <div className="max-w-(--breakpoint-xl) w-full flex flex-col lg:flex-row mx-auto items-center justify-between gap-y-14 gap-x-10 px-6 py-12 lg:py-0">
+        <div className="max-w-xl">
+          <Badge className="rounded-full py-1 border-none">
+            Just released v1.0.0
+          </Badge>
+          <h1 className="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl font-semibold leading-[1.2]! tracking-tight">
+            Lorem ipsum dolor sit amet consectetur!
+          </h1>
+          <p className="mt-6 max-w-[60ch] xs:text-lg">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita vero voluptatibus dolorum mollitia. Minima autem exercitationem consequatur officia esse, facilis velit porro eligendi, debitis, unde quia. Quos temporibus possimus architecto?
+          </p>
+          <div className="mt-12 flex flex-col sm:flex-row items-center gap-4">
+            <Button
+              size="lg"
+              className="w-full sm:w-auto rounded-full text-base"
+            >
+              Zamów <ArrowDownRightIcon className="h-5! w-5!" />
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              className="w-full sm:w-auto rounded-full text-base shadow-none"
+            >
+              <CirclePlayIcon className="h-5! w-5!" /> Zobacz Demo na YT
+            </Button>
+          </div>
+        </div>
+        <div className="relative lg:max-w-lg xl:max-w-xl w-full bg-accent rounded-xl aspect-square">
+          <Image
+            src="/laptop.jpg"
+            fill
+            alt="Laptop"
+            className="object-cover rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
